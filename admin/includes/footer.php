ba96b0a0bc933a039222b5a153762f9f060af77e
Khoa tester sieu cap vjp pro <nnakhoa.18it5@vku.udn.vn>
2021-01-08 02:40:03 +0700

    done

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>@<a href="#"></a></b>
+    </div>
+    <strong>Nhóm quản lý dự án Khoa-Khánh-Định-Huy-Bình</strong>
+</footer>
